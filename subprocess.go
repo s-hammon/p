0a998b2ac9633bc9d6166ec1b683a07a1e42ed94
sven <stevethebassguy@gmail.com>
2025-06-17 20:37:25 -0500

    polish

--- subprocess.go
+++ subprocess.go
@@ -6,10 +6,14 @@ import (
 	"context"
 	"fmt"
 	"io"
+	"log"
+	"os"
 	"os/exec"
 	"runtime"
 	"strings"
 	"sync"
+	"syscall"
+	"time"
 )
 
 type Scanner func(stderr bool, text string)
@@ -47,8 +51,10 @@ type Subprocess struct {
 	Stderr, Stdout, Combined     bytes.Buffer
 	StderrReader, StdoutReader   io.ReadCloser
 	stderrScanner, stdoutScanner *bufio.Scanner
+	Scanner                      Scanner
 	StdInWriter                  io.Writer
 	Nice, Pid                    int
+	Capture, Print               bool
 	Label                        Label
 }
 
@@ -57,7 +63,6 @@ func NewSubprocess(bin string, args ...string) (*Subprocess, error) {
 		Bin:  bin,
 		Args: args,
 		Env:  make(map[string]string),
-		Done: make(chan struct{}),
 	}
 
 	if !subp.ExecInPath() {
@@ -71,13 +76,11 @@ func (sp *Subprocess) SetArgs(args ...string) {
 }
 
 func (sp *Subprocess) ExecInPath() bool {
-	if _, err := exec.LookPath(sp.Bin); err != nil {
-		return false
-	}
-	return true
+	_, err := exec.LookPath(sp.Bin)
+	return err == nil
 }
 
-func (sp *Subprocess) Start(args ...string) error {
+func (sp *Subprocess) Start(args ...string) (err error) {
 	if len(args) > 0 {
 		sp.SetArgs(args...)
 	}
@@ -86,18 +89,48 @@ func (sp *Subprocess) Start(args ...string) error {
 	}
 
 	sp.Done = make(chan struct{})
-	sp.Cmd = exec.Command(sp.Bin, sp.Args...)
+
+	sp.Cmd = exec.CommandContext(sp.Context, sp.Bin, sp.Args...)
 	sp.Cmd.Dir = sp.WorkingDir
+
 	if sp.Env != nil {
 		sp.Cmd.Env = SerializeMap(sp.Env)
+	} else {
+		sp.Cmd.Env = os.Environ()
 	}
 
-	// TODO: add stdin/out/err readers
-	// TODO: trace, too?
-	err := sp.Cmd.Start()
+	sp.Stdout.Reset()
+	sp.Stderr.Reset()
+	sp.Combined.Reset()
+
+	sp.StdoutReader, err = sp.Cmd.StdoutPipe()
 	if err != nil {
 		return err
 	}
+	sp.StderrReader, err = sp.Cmd.StderrPipe()
+	if err != nil {
+		return err
+	}
+
+	sp.stderrScanner = bufio.NewScanner(sp.StderrReader)
+	sp.stderrScanner.Split(bufio.ScanLines)
+
+	sp.stdoutScanner = bufio.NewScanner(sp.StdoutReader)
+	sp.stdoutScanner.Split(bufio.ScanLines)
+
+	sp.StdInWriter, err = sp.Cmd.StdinPipe()
+	if err != nil {
+		return err
+	}
+
+	err = sp.Cmd.Start()
+	if err != nil {
+		sp.StdoutReader.Close()
+		sp.StderrReader.Close()
+		return err
+	}
+
+	sp.Pid = sp.Cmd.Process.Pid
 
 	go sp.scanAndWait()
 
@@ -119,14 +152,101 @@ func (sp *Subprocess) String() string {
 	return strings.Join(parts, " ")
 }
 
-// func (sp *Subprocess) scanAndWait() {
-// 	exitCh := make(chan bool)
-// 	label := sp.Label.Render()
-//
-// 	go func() {
-// 		for p
-// 	}()
-// }
+func (sp *Subprocess) scanAndWait() {
+	exitCh := make(chan bool)
+	label := sp.Label.Render()
+
+	// stderr
+	go func() {
+		for sp.stderrScanner.Scan() {
+			line := sp.stderrScanner.Text()
+			sp.mu.Lock()
+			if sp.Capture {
+				sp.Stderr.WriteString(line + "\n")
+				sp.Combined.WriteString(line + "\n")
+			}
+			if sp.Scanner != nil {
+				sp.Scanner(true, line)
+			}
+			if sp.Print {
+				if label != "" {
+					line = Format("%s | %s", Colorize(90, label), line)
+				}
+				fmt.Fprintf(os.Stderr, "%s", line+"\n")
+			}
+			sp.mu.Unlock()
+		}
+		exitCh <- true
+	}()
+
+	// stdout
+	go func() {
+		for sp.stdoutScanner.Scan() {
+			line := sp.stdoutScanner.Text()
+			sp.mu.Lock()
+			if sp.Capture {
+				sp.Stdout.WriteString(line + "\n")
+				sp.Combined.WriteString(line + "\n")
+			}
+			if sp.Scanner != nil {
+				sp.Scanner(false, line)
+			}
+			if sp.Print {
+				if label != "" {
+					line = Format("%s | %s", Colorize(90, label), line)
+				}
+				fmt.Fprintf(os.Stdout, "%s", line+"\n")
+			}
+			sp.mu.Unlock()
+		}
+		exitCh <- true
+	}()
+
+	err := sp.Cmd.Wait()
+	if err != nil {
+		sp.Err = err
+	}
+
+	<-exitCh
+	<-exitCh
+
+	sp.Done <- struct{}{}
+}
+
+func (sp *Subprocess) Run(args ...string) error {
+	err := sp.Start(args...)
+	if err != nil {
+		return err
+	}
+	err = sp.Wait()
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (sp *Subprocess) Wait() error {
+	select {
+	case <-sp.Done:
+	case <-sp.Context.Done():
+		sp.Cmd.Process.Signal(syscall.SIGINT)
+		t := time.NewTimer(5 * time.Second)
+		select {
+		case <-sp.Done:
+		case <-t.C:
+			log.Fatal(sp.Cmd.Process.Kill())
+		}
+	}
+	if sp.Err != nil {
+		return sp.Err
+	}
+	if sp.Cmd != nil && sp.Cmd.ProcessState != nil {
+		if code := sp.Cmd.ProcessState.ExitCode(); code != 0 {
+			return fmt.Errorf("exit code: %d", code)
+		}
+	}
+	return nil
+}
 
 type Label struct {
 	Value      string
