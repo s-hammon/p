a11a8b0297564289d3cc9579c74c3e4eac90813e
s-hammon <stevethebassguy@gmail.com>
2025-10-29 23:32:51 -0500

    added logging & simple stat functions

--- logging.go
+++ logging.go
@@ -1,5 +1,146 @@
 package p
 
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"os"
+	"sync"
+)
+
 func Colorize(color int, text string) string {
 	return Format("\x1b[%dm%v\x1b[0m", color, text)
+}
+
+// Logging specific to GCP
+type Severity int32
+
+const (
+	Debug Severity = iota + 1
+	Info
+	Notice
+	Warning
+	Error
+	Critical
+	Alert
+	Emergency
+)
+
+func (s Severity) MarshalJSON() ([]byte, error) {
+	switch s {
+	default:
+		return []byte(`"UNKNOWN"`), fmt.Errorf("unknown severity: %d", s)
+	case Debug:
+		return []byte(`"DEBUG"`), nil
+	case Info:
+		return []byte(`"INFO"`), nil
+	case Notice:
+		return []byte(`"NOTICE"`), nil
+	case Warning:
+		return []byte(`"WARNING"`), nil
+	case Error:
+		return []byte(`"ERROR"`), nil
+	case Critical:
+		return []byte(`"CRITICAL"`), nil
+	case Alert:
+		return []byte(`"ALERT"`), nil
+	case Emergency:
+		return []byte(`"EMERGENCY"`), nil
+	}
+}
+
+type Entry struct {
+	Message  string          `json:"message"`
+	Severity Severity        `json:"severity,omitempty"`
+	Trace    json.RawMessage `json:"logging.googleapis.com/trace,omitempty"`
+}
+
+type GcpLogger struct {
+	out, err io.Writer
+	mu       sync.Mutex
+	trace    json.RawMessage
+}
+
+func (l *GcpLogger) Print(v ...any) {
+	logGCP(Info, l, fmt.Sprint(v...))
+}
+
+func (l *GcpLogger) Debug(s string, v ...any) {
+	if len(v) > 0 {
+		s = Format(s, v...)
+	}
+	logGCP(Debug, l, s)
+}
+
+func (l *GcpLogger) Info(s string, v ...any) {
+	if len(v) > 0 {
+		s = Format(s, v...)
+	}
+	logGCP(Info, l, s)
+}
+
+func (l *GcpLogger) Notice(s string, v ...any) {
+	if len(v) > 0 {
+		s = Format(s, v...)
+	}
+	logGCP(Notice, l, s)
+}
+
+func (l *GcpLogger) Warning(s string, v ...any) {
+	if len(v) > 0 {
+		s = Format(s, v...)
+	}
+	logGCP(Warning, l, s)
+}
+
+func (l *GcpLogger) Error(s string, v ...any) {
+	if len(v) > 0 {
+		s = Format(s, v...)
+	}
+	logGCP(Error, l, s)
+}
+
+func (l *GcpLogger) Critical(s string, v ...any) {
+	if len(v) > 0 {
+		s = Format(s, v...)
+	}
+	logGCP(Critical, l, s)
+}
+
+func (l *GcpLogger) Alert(s string, v ...any) {
+	if len(v) > 0 {
+		s = Format(s, v...)
+	}
+	logGCP(Alert, l, s)
+}
+
+func (l *GcpLogger) Emergency(s string, v ...any) {
+	if len(v) > 0 {
+		s = Format(s, v...)
+	}
+	logGCP(Emergency, l, s)
+}
+
+func (l *GcpLogger) writer(s Severity) io.Writer {
+	if s >= Error {
+		if l.err != nil {
+			return l.err
+		}
+		return os.Stderr
+	}
+	if l.out != nil {
+		return l.out
+	}
+	return os.Stdout
+}
+
+func logGCP(s Severity, l *GcpLogger, msg string) string {
+	entry := Entry{msg, s, l.trace}
+	fmt.Println(entry)
+	enc := json.NewEncoder(l.writer(s))
+	enc.SetEscapeHTML(false)
+	l.mu.Lock()
+	defer l.mu.Unlock()
+	_ = enc.Encode(entry)
+	return msg
 }
--- stat/agg.go
+++ stat/agg.go
@@ -1,34 +1,56 @@
 package stat
 
-import "slices"
+import (
+	"math"
+	"slices"
+)
 
 type real interface {
-	int8 | int16 | int32 | int64 | float32 | float64
+	~int | ~int8 | ~int16 | ~int32 | ~int64 |
+		~uint | ~uint8 | ~uint16 | ~uint32 | ~uint64 |
+		~float32 | ~float64
 }
 
-func Mean[T real](a []T) T {
-	var sum T
+func Mean[T real](a []T) float64 {
 	if len(a) == 0 {
-		return sum
+		return 0
 	}
-	for _, num := range a {
-		sum += num
+	sum := 0.0
+	for _, v := range a {
+		sum += float64(v)
 	}
-	return sum / T(len(a))
+	return sum / float64(len(a))
 }
 
-func Median[T real](a []T) T {
-	var median T
+func Median[T real](a []T) float64 {
 	n := len(a)
 	if n == 0 {
-		return median
+		return 0
 	}
 	sorted := slices.Clone(a)
 	slices.Sort(sorted)
 
 	mid := n / 2
 	if n%2 == 1 {
-		return sorted[mid]
+		return float64(sorted[mid])
 	}
-	return (sorted[mid-1] + sorted[mid]) / 2
+	return (float64(sorted[mid-1]) + float64(sorted[mid])) / 2.0
+}
+
+func Stdev[T real](a []T) float64 {
+	n := len(a)
+	if n < 2 {
+		return 0
+	}
+
+	var mean, m2 float64
+	for i, v := range a {
+		x := float64(v)
+		k := float64(i + 1)
+		del := x - mean
+		mean += del / k
+		m2 += del * (x - mean)
+	}
+	variance := m2 / float64(n-1)
+	return math.Sqrt(variance)
 }
